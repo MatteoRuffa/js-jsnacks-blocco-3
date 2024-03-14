f473face8f613a58b40df4eee707b0e0bb88ed4d
MatteoRuffa <cortese_17@protonmail.com>
2024-03-14 11:37:35 +0100

    some correction

--- js/mr-script.js
+++ js/mr-script.js
@@ -1,9 +1,9 @@
 // Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
 // Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
 
-// Chiediamo all'utente quanti elementi vuole nell'array
-const numElements = parseInt(prompt('quanti elementi vuoi creare?'));
-console.log(numElements);
-
 // Creiamo un array vuoto
 const boxElements = [];
+
+// definiamo in mumeri da chieder all'utente
+let numerUser = parseInt(prompt('quanti elementi vuoi creare?'));
+console.log(numerUser);
