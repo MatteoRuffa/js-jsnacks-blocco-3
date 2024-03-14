895202491c617473a5ca116b73d3e82d13fa280b
MatteoRuffa <cortese_17@protonmail.com>
2024-03-14 11:44:27 +0100

    some try

--- js/mr-script.js
+++ js/mr-script.js
@@ -1,9 +1,25 @@
 // Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
-// Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+// Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando 
+// la somma degli elementi è minore di 50.
 
 // Creiamo un array vuoto
 const boxElements = [];
 
 // definiamo in mumeri da chieder all'utente
-let numerUser = parseInt(prompt('quanti elementi vuoi creare?'));
+let numerUser = parseInt(prompt('scegli un numero!'));
 console.log(numerUser);
+
+// creo un ciclo while per chieder n numeri fino a che laloro somma e inore di 50
+// (proviamo conun flag)
+sum = 0;
+while (true ) {
+    let numerUser = parseInt(prompt('scegli un numero!'));
+    console.log(numerUser);
+    if(!isNaN(num)) {
+        boxElements.push(numerUser);
+        sum += numerUser;
+        console.log(sum);
+    } else {
+        console.log("Per favore, inserisci un numero valido.");
+    }
+}
