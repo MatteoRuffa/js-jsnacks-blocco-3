9f54faa59361c7a39c8cf5b814f600da5d6d2c36
MatteoRuffa <cortese_17@protonmail.com>
2024-03-14 12:05:32 +0100

    add some start

---
+++ snack2/mr-script.js
@@ -0,0 +1,19 @@
+/**
+ * Snack3
+    Fai inserire un numero, che chiameremo N, all'utente.
+    Genera con una funzione N array, ognuno formato da 10 numeri casuali da 1 a 100.
+    Ogni volta che ne crei uno, stampalo.
+ */
+
+// Fai inserire un numero, che chiameremo N, all'utente.
+let numberUser = parseInt(prompt('inserisci il numero di Array che vuoi creare'));
+
+// Genera con una funzione N array, ognuno formato da 10 numeri casuali da 1 a 100.
+function generaArrayCasuale() {
+    let array = [];
+    for(let i = 0; i < 10; i++) {
+        array.push(Math.floor(Math.random() * 100) + 1);
+    }
+    return array;
+}
+
